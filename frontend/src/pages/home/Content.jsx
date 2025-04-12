b7dde85005eca382542dc6418632ef4e3fd23f9d
night-dreamer <charon.deadpan@gmail.com>
2025-04-12 10:51:18 +0800

    image testing

--- frontend/src/pages/home/Content.jsx
+++ frontend/src/pages/home/Content.jsx
@@ -4,11 +4,7 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 import EXPERIENCE from '../../utils/db/experience.js'
-
-import bumble from '../../assets/bumble.png';
-import soundSpace from '../../assets/soundspace.png';
-import gameParty from '../../assets/game-hunter.png';
-import scriptify from '../../assets/scriptify.png';
+import PROJECT from '../../utils/db/project.js';
 
 const Content = ({ aboutRef, experienceRef, projectsRef }) => {
   return (
@@ -42,10 +38,6 @@ const Content = ({ aboutRef, experienceRef, projectsRef }) => {
         transition={{ duration: 0.9 }}
       >
         <h2 className='font-bold text-slate-200 uppercase py-8 tracking-widest'>Experience</h2>
-        
-        <img src={bumble} alt='Bumble Bai' className='w-full h-auto object-cover rounded-md'/>
-        <img src={'../../../public/bumble.png'} alt='Bumble Bai' className='w-full h-auto object-cover rounded-md'/>
-
         {EXPERIENCE.map((experience, index) => (
           <>
             <Link to={experience.link} target="_blank" key={index}>
@@ -80,107 +72,30 @@ const Content = ({ aboutRef, experienceRef, projectsRef }) => {
       <section id='section3' ref={projectsRef} className='pt-10  lg:mt-20 flex flex-col gap-4'>
       <h2 className='font-bold text-slate-200 uppercase py-8 tracking-widest'>Projects</h2>
         
-      <Link to={"https://github.com/silv-error/bumble-bai"} target='_blank'>
-        <button
-          className='flex flex-col-reverse md:flex-row items-start gap-4 hover:bg-gray-700 hover:bg-opacity-50 backdrop-blur-md bg-opacity-25 rounded-md relative p-4 lg:right-4 group text-start hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg'>
-          <img src={bumble} className='max-w-40 rounded-sm mt-1'/>
-          <div>
-            <h2 className='font-medium text-slate-300 group-hover:text-cyan-300 lg:mt-0 flex group gap-2'>
-              Bumble Bai
-              <span className='hover:transform translate-y-1.5 -translate-x-1 group-hover:-translate-y-0 group-hover:translate-x-0 transition-all duration-900'>
-                <ArrowUpRight size={16}/>
-              </span>
-            </h2>
-            <p className='text-slate-400 mt-2 text-sm'>
-            A unique online market platform that mimics the swipe-based interaction of dating apps like Bumble.
-            </p>
-            <div className='flex flex-wrap gap-1 mt-2'>
-              <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Node.js</div>
-              <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>React</div>
-              <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>MongoDB</div>
-              <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Express</div>
-            </div>
-          </div>
-        </button>
-      </Link>
-
-      <button
-        className='flex flex-col-reverse md:flex-row items-start gap-4 hover:bg-gray-700 hover:bg-opacity-50 backdrop-blur-md bg-opacity-25 rounded-md relative p-4 lg:right-4 group text-start hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg'>
-        <img src={soundSpace} className='max-w-40 rounded-sm mt-1'/>
-        <div>
-          <h2 className='font-medium text-slate-300 group-hover:text-cyan-300 lg:mt-0 flex group gap-2'>
-            Sound Space
-            <span className='hover:transform translate-y-1.5 -translate-x-1 group-hover:-translate-y-0 group-hover:translate-x-0 transition-all duration-900'>
-              <ArrowUpRight size={16}/>
-            </span>
-          </h2>
-          <p className='text-sm font-medium text-blue-200 mt-2 flex items-center gap-1'>
-            <CircleAlert size={20} /> Not yet hosted.
-          </p>
-          <p className='text-slate-400 mt-2 text-sm'>
-            The platform displays basic user’s info, such as: 
-            following artists, recently played songs, today's biggest hit artists, and saved albums.
-          </p>
-          <div className='flex flex-wrap gap-1 mt-2'>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Python</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Flask</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>HTML</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>CSS</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Bootstrap</div>
-          </div>
-        </div>
-      </button>
-
-      <button
-        className='flex flex-col-reverse md:flex-row items-start gap-4 hover:bg-gray-700 hover:bg-opacity-50 backdrop-blur-md bg-opacity-25 rounded-md relative p-4 lg:right-4 group text-start hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg'>
-        <img src={scriptify} className='max-w-40 rounded-sm mt-1'/>
-        <div>
-          <h2 className='font-medium text-slate-300 group-hover:text-cyan-300 lg:mt-0 flex group gap-2'>
-            Scriptify
-            <span className='hover:transform translate-y-1.5 -translate-x-1 group-hover:-translate-y-0 group-hover:translate-x-0 transition-all duration-900'>
-              <ArrowUpRight size={16}/>
-            </span>
-          </h2>
-          <p className='text-sm font-medium text-blue-200 mt-2 flex items-center gap-1'>
-            <CircleAlert size={20} /> Not yet hosted.
-          </p>
-          <p className='text-slate-400 mt-2 text-sm'>
-            PDF to MP3; Users can select their preferred voice options before the conversion, and the resulting audio can be downloaded.
-          </p>
-          <div className='flex flex-wrap gap-1 mt-2'>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Python</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Flask</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>HTML</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>CSS</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Tailwind</div>
-          </div>
-        </div>
-      </button>
-
-      <button
-        className='flex flex-col-reverse md:flex-row items-start gap-4 hover:bg-gray-700 hover:bg-opacity-50 backdrop-blur-md bg-opacity-25 rounded-md relative p-4 lg:right-4 group text-start hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg'>
-        <img src={gameParty} className='max-w-40 rounded-sm mt-1'/>
-        <div>
-          <h2 className='font-medium text-slate-300 group-hover:text-cyan-300 lg:mt-0 flex group gap-2'>
-            Game Hunter
-            <span className='hover:transform translate-y-1.5 -translate-x-1 group-hover:-translate-y-0 group-hover:translate-x-0 transition-all duration-900'>
-              <ArrowUpRight size={16}/>
-            </span>
-          </h2>
-          <p className='text-sm font-medium text-blue-200 mt-2 flex items-center gap-1'>
-            <CircleAlert size={20} /> Not yet hosted.
-          </p>
-          <p className='text-slate-400 mt-2 text-sm'>
-            Game Party Finder is a social platform designed to help gamers connect with others and organize gaming sessions.
-          </p>
-          <div className='flex flex-wrap gap-1 mt-2'>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Node.js</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>React</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>MongoDB</div>
-            <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>Express</div>
-          </div>
-        </div>
-      </button>
+        {PROJECT.map((project, index) => (
+          <Link to={project.link} key={index}>
+            <button
+              className='flex flex-col-reverse md:flex-row items-start gap-4 hover:bg-gray-700 hover:bg-opacity-50 backdrop-blur-md bg-opacity-25 rounded-md relative p-4 lg:right-4 group text-start hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] hover:drop-shadow-lg'>
+              <img src={project.img} className='max-w-40 rounded-sm mt-1'/>
+              <div>
+                <h2 className='font-medium text-slate-300 group-hover:text-cyan-300 lg:mt-0 flex group gap-2'>
+                  {project.title}
+                  <span className='hover:transform translate-y-1.5 -translate-x-1 group-hover:-translate-y-0 group-hover:translate-x-0 transition-all duration-900'>
+                    <ArrowUpRight size={16}/>
+                  </span>
+                </h2>
+                <p className='text-slate-400 mt-2 text-sm'>
+                  {project.description}
+                </p>
+                <div className='flex flex-wrap gap-1 mt-2'>
+                  {project.techstack.map((tech) => (
+                    <div className='badge bg-cyan-300 bg-opacity-20 p-3 text-cyan-300 font-medium border-0 text-xs'>{tech}</div>
+                  ))}
+                </div>
+              </div>
+            </button>
+          </Link>
+        ))}
 
         <footer className="footer text-base-content my-20">
           <aside>
---
+++ frontend/src/utils/db/project.js
@@ -0,0 +1,37 @@
+const PROJECT = [
+  {
+    img: require("../../assets/bumble.png"),
+    title: "Bumble Bai",
+    hosted: true,
+    description: "A unique online market platform that mimics the swipe-based interaction of dating apps like Bumble.",
+    link: "https://github.com/silv-error/bumble-bai",
+    techstack: ["Node.js", "React", "MongoDB", "Express"]
+  },
+  {
+    img: require("../../assets/soundspace.png"),
+    title: "Sound Space",
+    hosted: false,
+    description: `The platform displays basic user’s info, such as: 
+              following artists, recently played songs, today's biggest hit artists, and saved albums.`,
+    link: "",
+    techstack: ["Python", "Flask", "HTML", "CSS", "Boostrap"]
+  },
+  {
+    img: require("../../assets/scriptify.png"),
+    title: "Scriptify",
+    hosted: false,
+    description: "PDF to MP3; Users can select their preferred voice options before the conversion, and the resulting audio can be downloaded.",
+    link: "",
+    techstack: ["Python", "Flask", "HTML", "CSS", "Tailwind"]
+  },
+  {
+    img: require("../../assets/game-hunter.png"),
+    title: "Game Hunter",
+    hosted: false,
+    description: "Game Party Finder is a social platform designed to help gamers connect with others and organize gaming sessions.",
+    link: "",
+    techstack: ["Node.js", "React", "MongoDB", "Express"]
+  },
+]
+
+export default PROJECT;
